ed664bda280af845189bf3862e5dfec0fb4baa28
ragul <ragulraja893@gmail.com>
2024-05-14 12:18:41 +0530

    second

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zyt9m*6#=medz_89(g9utd^0#iyb7^wid#q_=lgc72vqg_0!@e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
